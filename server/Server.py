62abce4e6bb3decb7429b1c38ef4da57fd7f048f
Alonc2500 <alonc2500@gmail.com>
2015-03-16 19:59:31 +0200

    poop

--- server/Server.py
+++ server/Server.py
@@ -2,9 +2,10 @@ import socket
 import thread
 
 BUFFER_SIZE=1024
-TCP_IP= socket.gethostbyname(socket.gethostname())
-TCP_PORT=3456
+TCP_IP= "25.164.130.160"
+TCP_PORT=3334
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Server Working"
 s.bind((TCP_IP,TCP_PORT))
 s.listen(3)
 data="NO_DATA"
