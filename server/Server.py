b0a64e51d40523829ed0a473cf8b1c9b4323559d
Foxman <ron.mikhlin7@gmail.com>
2015-03-16 20:58:55 +0200

    added first client handeling

--- server/Server.py
+++ server/Server.py
@@ -2,17 +2,52 @@ import socket
 import thread
 
 BUFFER_SIZE=1024
-TCP_IP= "25.164.130.160"
+TCP_IP= "25.86.198.77"
 TCP_PORT=3334
+data="NO_DATA"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Server Working"
+    
 s.bind((TCP_IP,TCP_PORT))
 s.listen(3)
-data="NO_DATA"
-msg="NONE"
-conn, addr = s.accept()
-print "Connection Address:" , addr
-while data != "exit":
-    data = conn.recv(BUFFER_SIZE)
-    print data
-conn.close()
+
+conn1, addr2 = s.accept()
+print "Player 1 connected"
+
+
+conn2, addr2 = s.accept()
+print "Player 2 connected"
+
+      
+def main():
+    try:
+        thread.start_new_thread( recieve, (conn1, 1))
+    except:
+        print "Player 1 failed to start!"
+        
+
+    try:
+        thread.start_new_thread( recieve, (conn2, 2))
+    except:
+        print "Player 2 failed to start!"
+    
+
+    
+def recieve (conn, player):
+    print "player %d started playing"%(player)
+    data = "no data"
+    
+    while data != "exit":
+        data = conn.recv(BUFFER_SIZE)
+        print data
+
+        if(player == 1):
+            conn2.send(data)
+        else:
+            conn1.send(data)
+        
+    conn.close()  
+
+if __name__ == "__main__":
+  main()
